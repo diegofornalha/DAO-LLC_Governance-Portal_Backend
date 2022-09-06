ece0e044f60683b81932d2bfc9bfd1762d66fa8e
BoiseITGuru <dev@boiseitguru.com>
2022-09-06 03:47:26 -0600

    poc work

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,53 @@
 const functions = require("firebase-functions");
+const { initializeApp } = require('firebase-admin/app')
+const { getAuth } = require('firebase-admin/auth')
+const { getFirestore } = require('firebase-admin/firestore')
+const fcl = require("@onflow/fcl");
+const crypto = require("crypto");
+const { database } = require("firebase-functions/v1/firestore");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+fcl.config({
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "flow.network": "testnet"
+})
+
+initializeApp();
+const db = getFirestore();
+
+exports.generateNonce = functions.https.onCall( async (data, context) => {
+    const nonce = crypto.randomBytes(32).toString('hex');
+
+    await db.collection('nonce-tokens').add({
+        nonce: nonce
+    })
+
+    return {
+        nonce: nonce
+    }
+})
+
+exports.generateAuthToken = functions.https.onCall( async (data, context) => {
+    let nonceDoc = await db.collection('nonce-tokens').where('nonce', '=', data.nonce).get()
+    if (nonceDoc.empty) {
+        return {
+            status: "error",
+            msg: "Invalid Nonce"
+        }
+    }
+
+    const isValid = await fcl.AppUtils.verifyAccountProof("DAO LLC Governance Portal (v0.1)", data)
+
+    if (!isValid) {
+        return {
+            status: "error",
+            msg: "Invalid Account Proof"
+        }
+    }
+
+    let token = await getAuth().createCustomToken(data.address)
+
+    return {
+        status: "success",
+        token: token
+    }
+})
